3113101b0689b6107be468bb7c7de99fe35896c2
Anton Sapunov <a.sapunov@hackspace.by>
2021-06-26 15:26:11 +0300

    [*] Move colors to constants

--- MoneyKeeper/DesignSystem/+Color.swift
+++ MoneyKeeper/DesignSystem/+Color.swift
@@ -12,7 +12,14 @@ extension Color {
     static let background = Color("background")
     
     enum Button {
-        
+        static let foreground = Color("button_foreground")
+        static let background = Color("button_background")
+    }
+    
+    enum TextField {
+        static let foreground = Color("textfield_foreground")
+        static let accent = Color("textfield_accent")
+        static let background = Color("textfield_background")
     }
     
     enum Icon {
