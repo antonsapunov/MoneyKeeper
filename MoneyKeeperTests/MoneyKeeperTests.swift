575a9463ad5f3125b386f1aa3e01b4677aa93863
Anton Sapunov <a.sapunov@hackspace.by>
2022-06-04 16:54:39 +0300

    Init tests

---
+++ MoneyKeeperTests/MoneyKeeperTests.swift
@@ -0,0 +1,32 @@
+//
+//  MoneyKeeperTests.swift
+//  MoneyKeeperTests
+//
+//  Created by Anton Sapunov on 6/4/22.
+//
+
+import XCTest
+import RealmSwift
+@testable import MoneyKeeper
+
+class MoneyKeeperTests: XCTestCase {
+    
+    var realm: Realm!
+
+    override func setUpWithError() throws {
+        let realm = try! Realm()
+        self.realm = realm
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_CategoryCount_Initially_ShouldBeZero() {
+        XCTAssertEqual(realm.objects(RealmCategory.self).count, 5, "should be 5 (Initial categories count)")
+    }
+    
+    func test_TransactionsCount_Initially_ShouldBeZero() {
+        XCTAssertEqual(realm.objects(RealmTransaction.self).count, 0, "should be 0")
+    }
+}
